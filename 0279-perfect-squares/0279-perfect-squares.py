3988f07b43272ded9615cd2906b9d51a8097dffa
Jay Cho <112677261+jay0718@users.noreply.github.com>
2022-11-22 19:44:44 -0500

    Time: 48 ms (90.04%), Space: 13.4 MB (93.68%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,14 +1,11 @@
-from math import isqrt
-
-class Solution:
-    def numSquares(self , n):
-        if isqrt(n)**2 == n:
-            return 1
-        for i in range(1,isqrt(n)+1):                
-            if (j := n - i**2) == isqrt(j)**2:
-                return 2
-        while n % 4 == 0:
-            n /= 4
-        if n % 8 != 7: 
-            return 3
-        return 4
+class Solution(object):
+    def numSquares(self, n):
+        while( n % 4 == 0 ):
+            n //= 4
+        if n % 8 == 7:
+            return 4
+        for a in range( int(sqrt(n))+1 ):
+            b = int( sqrt( n - a*a ) )
+            if ( a**2 + b ** 2 ) == n :
+                return (a>0) + (b>0)
+        return 3
